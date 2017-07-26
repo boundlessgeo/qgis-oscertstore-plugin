8309d20709a5f85ad7648297c2746be5b3beb44b
Alessandro Pasotti <apasotti@boundlessgeo.com>
2017-07-26 16:16:25 +0200

    Renamed logger to plugin and added translation support

--- oscertstore/certs_importer.py
+++ oscertstore/certs_importer.py
@@ -25,21 +25,21 @@ from qgis.PyQt.QtCore import QByteArray
 from qgis.PyQt.QtNetwork import QSslCertificate
 
 
-def run(logger):
+def run(plugin):
     """Import intermediate certs and return True on success"""
     
     if QgsAuthManager.instance().isDisabled():
-        logger.log(QgsAuthManager.instance().disabledMessage())
+        plugin.log(QgsAuthManager.instance().disabledMessage())
         return False
 
     ca_pems = dict()
     with wincertstore.CertSystemStore("CA") as store:
         for cert in store.itercerts(usage=None):
-            # logger.log(cert.get_name())
-            # logger.log(cert.enhanced_keyusage_names())
+            # plugin.log(cert.get_name())
+            # plugin.log(cert.enhanced_keyusage_names())
             ca_pems[cert.get_name()] = cert.get_pem().decode("ascii")
 
-    logger.log("Number of possible CAs found: {0}".format(len(ca_pems)))
+    plugin.log(plugin.tr("Number of possible CAs found: {0}").format(len(ca_pems)))
 
     if not ca_pems:
         return False
@@ -50,34 +50,34 @@ def run(logger):
     for ca_cn, ca_pem in ca_pems.items():
         pem_ba = QByteArray(ca_pem)
         cas = QSslCertificate.fromData(pem_ba)
-        #logger.log("Converted PEM to QSslCertificate {0}: ".format(ca_cn))
+        #plugin.log("Converted PEM to QSslCertificate {0}: ".format(ca_cn))
         if not cas:
-            logger.log("Could not convert PEM to QSslCertificate: {0}".format(ca_cn))
+            plugin.log(plugin.tr("Could not convert PEM to QSslCertificate: {0}").format(ca_cn))
             continue
 
         ca = cas[0]
         if not ca.isValid():
-            logger.log("  cert not valid: {0}".format(ca_cn))
+            plugin.log(plugin.tr("  cert not valid: {0}").format(ca_cn))
             continue
         if not QgsAuthCertUtils.certificateIsAuthority(ca):
-            logger.log("  cert not a CA: {0}".format(ca_cn))
+            plugin.log(plugin.tr("  cert not a CA: {0}").format(ca_cn))
             continue
         if ca in trusted_cas:
-            logger.log("  cert already in trusted CA cache: {0}".format(ca_cn))
+            plugin.log(plugin.tr("  cert already in trusted CA cache: {0}").format(ca_cn))
             continue
-        logger.log("  found CA to add: {0}".format(ca_cn))
+        plugin.log(plugin.tr("  found CA to add: {0}").format(ca_cn))
         ca_certs.append(ca)
 
     if ca_certs:
-        logger.log("Storing CAs in auth system db")
+        plugin.log(plugin.tr("Storing CAs in auth system db"))
         if not QgsAuthManager.instance().storeCertAuthorities(ca_certs):
-            logger.log("  FAILED")
+            plugin.log(plugin.tr("  FAILED"))
             return False
-        logger.log("  SUCCESS")
-        logger.log("Reinitializing auth system SSL caches")
+        plugin.log(plugin.tr("  SUCCESS"))
+        plugin.log(plugin.tr("Reinitializing auth system SSL caches"))
         QgsAuthManager.instance().initSslCaches()
         return True
     else:
-        logger.log("No CAs found to store in auth system db")
+        plugin.log(plugin.tr("No CAs found to store in auth system db"))
     return True
 
