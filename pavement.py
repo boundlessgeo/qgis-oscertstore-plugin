c71aba2da4a90fb2e0bbe3a58095b8609a8877fa
Luigi Pirelli <luipir@gmail.com>
2017-08-22 16:16:52 +0200

    ext-libs to extlibs

--- pavement.py
+++ pavement.py
@@ -21,7 +21,7 @@ from paver.doctools import html
 options(
     plugin = Bunch(
         name = 'oscertstore',
-        ext_libs = path('oscertstore/ext-libs'),
+        ext_libs = path('oscertstore/extlibs'),
         source_dir = path('oscertstore'),
         package_dir = path('.'),
         tests = ['test', 'tests'],
@@ -225,7 +225,7 @@ def pep8(args):
     ignore = ['E203', 'E121', 'E122', 'E123', 'E124', 'E125', 'E126', 'E127',
         'E128', 'E402']
     styleguide = pep8.StyleGuide(ignore=ignore,
-                                 exclude=['*/ext-libs/*', '*/ext-src/*'],
+                                 exclude=['*/extlibs/*', '*/ext-src/*'],
                                  repeat=True, max_line_length=79,
                                  parse_argv=args)
     styleguide.input_dir(options.plugin.source_dir)
